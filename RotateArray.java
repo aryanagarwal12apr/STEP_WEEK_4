ea4b950ec2ed4f2a046b5f59a857b23bdf1d21d4
Aryan Agarwal <aryanagarwal12apr@gmail.com>
2026-09-12 14:48:39 +0530

    Add array rotation functionality in RotateArray class
    Implement array rotation logic and main method to test it.

--- RotateArray.java
+++ RotateArray.java
@@ -1,1 +1,37 @@
+public class RotateArray {
 
+    public static int[] rotateArray(int[] nums, int k) {
+
+        int n = nums.length;
+
+        k = k % n;
+
+        int[] newArray = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            newArray[(i + k) % n] = nums[i];
+        }
+
+        return newArray;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 4, 5, 6, 7};
+        int k = 3;
+
+        int[] result = rotateArray(nums, k);
+
+        System.out.print("[");
+
+        for (int i = 0; i < result.length; i++) {
+            System.out.print(result[i]);
+
+            if (i < result.length - 1) {
+                System.out.print(", ");
+            }
+        }
+
+        System.out.println("]");
+    }
+}
